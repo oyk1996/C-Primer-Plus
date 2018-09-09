7c16ae25e1e72d2e2637ed1ea36ef3eb35c509d7
oyk1996 <oyk9607@gmail.com>
2018-09-09 12:00:24 +0800

    layout

---
+++ Chapter_01/2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("ĞÕÃû£º¾®É­Ñô\n");
+    printf("µØÖ·£ºhttps://github.com/oyk1996");
+
+    return 0;
+}
---
+++ Chapter_01/3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+    int ago = 22;
+    int day;
+    day = ago *365;
+    printf("我 %d 岁啦\n", ago);
+    printf("我出生 %d 天啦\n",day);
+
+    return 0;
+}
